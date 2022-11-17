31abb48a006fc8d6075fbe0fbcc855006f254a0a
Slava <loginmail2018@inbox.ru>
2022-11-17 14:38:06 +0300

    комменты

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -3,7 +3,11 @@ package app.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = "app")
+
+@Configuration /*это новый способ настройки
+                Spring с использованием аннотаций вместо XML-файлов (это называется конфигурацией Java)*/
+@ComponentScan(basePackages = "app") // сообщаем спрингу, что в пакете "app"
+                                    // есть аннотированные классы, которые должны управляться Spring-ом
+
 public class AppConfig {
 }
